f00823109c4eb86db42549b0573761e302e9f956
Jaime López <114154176+Jaime1999l@users.noreply.github.com>
2024-09-20 00:46:01 +0200

    -

--- src/main/java/io/teamsgroup/caso_1_programacion_concurrente/controller/WebController.java
+++ src/main/java/io/teamsgroup/caso_1_programacion_concurrente/controller/WebController.java
@@ -2,7 +2,6 @@ package io.teamsgroup.caso_1_programacion_concurrente.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class WebController {
