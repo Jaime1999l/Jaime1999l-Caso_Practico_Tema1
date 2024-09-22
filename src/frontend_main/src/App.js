294c774a25c7408622ccec07d927070bae9b841c
Jaime López <114154176+Jaime1999l@users.noreply.github.com>
2024-09-22 13:29:12 +0200

    CSS
    Pagina principal

--- src/frontend_main/src/App.js
+++ src/frontend_main/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './hocs/pages/LoginPage';
+import PrincipalPage from './hocs/pages/PrincipalPage';
 import AdminDashboard from './hocs/pages/AdminDashboard';
 import UserDashboard from './hocs/pages/UserDashboard';
 
@@ -8,7 +8,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<LoginPage />} />
+                <Route path="/" element={<PrincipalPage />} />
                 <Route path="/admin" element={<AdminDashboard />} />
                 <Route path="/user" element={<UserDashboard />} />
             </Routes>
